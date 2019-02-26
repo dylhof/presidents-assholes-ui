3d696bb6d51d6301951e7f8407aeaf4fcd0cc3a8
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-02-26 09:57:10 -0700

    Add reducers and add them to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux'
-
+import { loadingReducer, errorReducer, presidentsReducer } from './reducers'
 
 const rootReducer = combineReducers({
-
+  isLoading: loadingReducer,
+  error: errorReducer,
+  presidents: presidentsReducer
 })
 
 export default rootReducer;
---
+++ src/reducers/reducers.js
@@ -0,0 +1,26 @@
+export const loadingReducer = (state = false, action) => {
+  switch(action.type) {
+    case 'SET_Loading':
+      return action.isLoading;
+    default:
+      return state;
+  }
+}
+
+export const errorReducer = (state = '', action) => {
+  switch(action.type) {
+    case 'SET_ERROR':
+      return action.error;
+    default:
+      return state;
+  }
+}
+
+export const presidentsReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'SET_PRESIDENTS':
+      return action.presidents;
+    default:
+      return state;
+  }
+}
